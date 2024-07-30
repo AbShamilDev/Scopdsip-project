c5b3900c0df6fee82c80bf3473f487b3e5d9f27d
AbShamilDev <abkarov02@gmail.com>
2024-07-30 06:21:28 +0300

    add loading anim demo page

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
-import './App.css';
-import Authorization from './pages/AuthorizationPage/Authorization';
-import { BrowserRouter, Route, Routes, useNavigate } from 'react-router-dom';
-import MainPage from './pages/MainPage/MainPage';
-
+import "./App.css";
+import Authorization from "./pages/AuthorizationPage/Authorization";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import MainPage from "./pages/MainPage/MainPage";
+import Loading from "./components/Loading/Loading";
 
 function App() {
-  console.log(localStorage.getItem('user'))
+  console.log(localStorage.getItem("user"));
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Authorization />} />
           <Route path="/main/*" element={<MainPage />} />
+          <Route path="/loading" element={<Loading />} />
         </Routes>
       </BrowserRouter>
     </div>
